79c82eed1c3c3127dbcbf49739507487e0bd5003
Annexiaojin <2900936859@qq.com>
2023-12-20 17:53:50 +0800

    添加新增

--- src/main/java/com/article/controller/UserController.java
+++ src/main/java/com/article/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.article.controller;
 
 import com.article.entity.User;
+import com.article.properties.UserProperties;
 import com.article.service.UserService;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -12,10 +13,22 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserProperties userProperties;
     @RequestMapping("/query")
     public List<User> queryAll(){
         List<User> users = userService.queryAll();
         System.out.println(users);
         return users;
+    }
+    @RequestMapping("/queryOne/{id}")
+    public User queryOne(@PathVariable int id){
+        User user = userService.queryOne(id);
+        System.out.println(user);
+        return user;
+    }
+    @RequestMapping("/insert")
+    void insertUser(){
+        userService.insertUser(userProperties);
     }
 }
