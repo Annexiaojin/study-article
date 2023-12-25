c1762e5f03338848380c43ce73d8baf9cc1f8646
Annexiaojin <2900936859@qq.com>
2023-12-25 16:04:10 +0800

    validation校验的引入

--- src/main/java/com/article/controller/UserController.java
+++ src/main/java/com/article/controller/UserController.java
@@ -4,13 +4,16 @@ import com.article.entity.Result;
 import com.article.entity.User;
 import com.article.properties.UserProperties;
 import com.article.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 @RestController
+@Validated
 @RequestMapping("/user")
 public class UserController {
     @Autowired
@@ -29,13 +32,14 @@ public class UserController {
         userService.insertUser(userProperties);
     }
     @PostMapping("/register")
-    public Result register(String userName,String password){
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String userName,@Pattern(regexp = "^\\S{5,16}$")String password){
         User user = User.builder().name(userName).password(password).build();
-        User u = userService.queryOne(user);
-        if (u == null){
-            return Result.success();
-        }else {
-            return Result.error("用户名已存在");
-        }
+            User u = userService.queryOne(user);
+            if (u == null){
+                return Result.success();
+            }else {
+                return Result.error("用户名已存在");
+            }
+
     }
 }
